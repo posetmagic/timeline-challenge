75301175efb99960a5e3d975e918af76a1244288
Poset Mage <mail@posetmage.com>
2024-10-17 12:53:06 +0000

    playhead follow current

--- src/Timeline/Playhead.tsx
+++ src/Timeline/Playhead.tsx
@@ -3,17 +3,30 @@
 import { useSelector } from "react-redux";
 
 export const Playhead = () => {
-  
-  // Use useSelector to get the time from the Redux store
+  // Use useSelector to get the time and scroll from the Redux store
   const time = useSelector((state: any) => state.time.current);
+  const horizontalScroll = useSelector((state: any) => state.scroll.horizontal); // Get horizontal scroll position from Redux
+
+  // Calculate the actual position of the playhead
+  const playheadPosition = time - horizontalScroll;
+
+  // Determine the threshold for hiding the playhead
+  const threshold = 316;
+
+  // Check if the playhead should be hidden
+  const isHidden = playheadPosition < 0 || playheadPosition > threshold;
 
   return (
-    <div
-      className="absolute left-[316px] h-full border-l-2 border-solid border-yellow-600 z-10"
-      data-testid="playhead"
-      style={{ transform: `translateX(calc(${time}px - 50%))` }}
-    >
-      <div className="absolute border-solid border-[5px] border-transparent border-t-yellow-600 -translate-x-1.5" />
-    </div>
+    <>
+      {!isHidden && (
+        <div
+          className="absolute left-[316px] h-full border-l-2 border-solid border-yellow-600 z-10"
+          data-testid="playhead"
+          style={{ transform: `translateX(calc(${playheadPosition}px - 50%))` }}
+        >
+          <div className="absolute border-solid border-[5px] border-transparent border-t-yellow-600 -translate-x-1.5" />
+        </div>
+      )}
+    </>
   );
 };
