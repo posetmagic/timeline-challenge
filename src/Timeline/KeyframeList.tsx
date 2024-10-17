90b6f079dc9db5e57b4b1d5fcf58be057d541ba6
Poset Mage <mail@posetmage.com>
2024-10-17 11:45:26 +0000

    fix virtical scroll right

--- src/Timeline/KeyframeList.tsx
+++ src/Timeline/KeyframeList.tsx
@@ -1,24 +1,33 @@
 // KeyframeList.tsx
+
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setHorizontalScroll } from './State.store'; // Adjust the import path as needed
+import { setHorizontalScroll, setVerticalScroll } from './State.store'; // Import setVerticalScroll
 import { Segment } from "./Segment";
 
 export const KeyframeList = () => {
   const dispatch = useDispatch();
-  const horizontalScroll = useSelector((state: any) => state.scroll.horizontal); // Replace 'any' with your state type
+  const horizontalScroll = useSelector((state: any) => state.scroll.horizontal);
+  const verticalScroll = useSelector((state: any) => state.scroll.vertical);
 
   useEffect(() => {
-    // Set the scroll position when the component mounts
     const container = document.getElementById('keyframe-list');
     if (container) {
       container.scrollLeft = horizontalScroll;
     }
   }, [horizontalScroll]);
 
+  useEffect(() => {
+    const container = document.getElementById('keyframe-list');
+    if (container) {
+      container.scrollTop = verticalScroll; // Synchronize vertical scroll position
+    }
+  }, [verticalScroll]);
+
   const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
-    const { scrollLeft } = event.currentTarget;
+    const { scrollLeft, scrollTop } = event.currentTarget;
     dispatch(setHorizontalScroll(scrollLeft));
+    dispatch(setVerticalScroll(scrollTop)); // Dispatch the vertical scroll position
   };
 
   return (
